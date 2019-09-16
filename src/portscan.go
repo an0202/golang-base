86aa92a6eac34e34071bdbda768a87c1b76b0281
Jie An <jie.an@bespingchina.cn>
2019-09-16 17:54:48 +0800

    update  log and descirbeport for portscan

--- src/portscan.go
+++ src/portscan.go
@@ -7,6 +7,7 @@ import (
 	"regexp"
 	"sync"
 	"time"
+	"tools"
 )
 
 // PortScanner Object
@@ -19,23 +20,21 @@ type PortScanner struct {
 
 // Check Configuration
 func (ps *PortScanner) Check(host string, startPort, endPort int32) {
-	fmt.Println("[INFO] Configure Checking  ...")
+	tools.InfoLogger.Println("Configure Checking  ...")
 	match, _ := regexp.MatchString("((?:(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d))\\.){3}(?:25[0-5]|2[0-4]\\d|(?:1\\d{2}|[1-9]?\\d)))", host)
 	if match == false {
-		fmt.Printf("[ERROR] %s Is Not A Ip Address\n", host)
-		os.Exit(2)
+		tools.ErrorLogger.Fatalf("%s Is Not A Ip Address\n", host)
 	}
 	if startPort > endPort {
-		fmt.Println("[ERROR] endPort Must Greater Than Or Equal To startPort")
-		os.Exit(2)
+		tools.ErrorLogger.Fatalln("endPort Must Greater Than Or Equal To startPort")
 	}
-	fmt.Println("[INFO] Configuration Check Successfully")
+	tools.InfoLogger.Println("Configuration Check Successfully")
 }
 
 // Scan Func
 func (ps *PortScanner) Scan(host string, startPort, endPort int32) {
 	wg := sync.WaitGroup{}
-	fmt.Printf("[INFO] Scanning Host %s Port %d To %d ...\n", host, startPort, endPort)
+	tools.InfoLogger.Printf("Scanning Host %s Port %d To %d ...\n", host, startPort, endPort)
 	for port := startPort; port <= endPort; port++ {
 		wg.Add(1)
 		go func(port int32) {
@@ -54,20 +53,35 @@ func (ps *PortScanner) Scan(host string, startPort, endPort int32) {
 		}(port)
 	}
 	wg.Wait()
-	fmt.Printf("[INFO] Scanning Is Over And %d Available Ports Detected\n", len(ps.availablePorts))
-	fmt.Printf("[INFO] Available Ports Is : %d\n", ps.availablePorts)
+	tools.InfoLogger.Printf("%d Available Ports Detected\n", len(ps.availablePorts))
+	tools.InfoLogger.Printf("Available Ports Is : %d\n", ps.availablePorts)
+}
+
+func (ps *PortScanner) descriebPorts() {
+	if len(ps.availablePorts) == 0 {
+		os.Exit(1)
+	}
+	for _, port := range ps.availablePorts {
+		result := tools.DescribePort(port)
+		if result != "" {
+			fmt.Printf("%-5d --> %6s\n", port, result)
+		} else {
+			fmt.Printf("%-5d --> %6s\n", port, "unknown Port")
+		}
+	}
 }
 
 func main() {
-	fmt.Println("[INFO] Task Start")
+	tools.InfoLogger.Println("Task Start")
 	ps := new(PortScanner)
-	ps.ipAddr = "127.0.0.1"
-	ps.startPort = 1000
-	ps.endPort = 2000
+	ps.ipAddr = "10.250.101.254"
+	ps.startPort = 1
+	ps.endPort = 65535
 	ps.Check(ps.ipAddr, ps.startPort, ps.endPort)
 	ps.Scan(ps.ipAddr, ps.startPort, ps.endPort)
-	fmt.Println("[INFO] Task Done")
+	ps.descriebPorts()
+	tools.InfoLogger.Println("Task Done")
 }
 
 // Todo list:
-// 排序实现，端口对应实现，命令行参数添加，时间统计，日志
+// 排序实现，命令行参数添加
